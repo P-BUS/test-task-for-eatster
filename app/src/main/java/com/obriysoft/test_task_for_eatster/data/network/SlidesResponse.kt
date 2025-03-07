de1e74da50f8f195b99d41750363fab88fe94770
P-BUS <pavlo.buslowski@gmail.com>
2025-03-07 21:24:24 +0200

    - internet permission added

--- app/src/main/java/com/obriysoft/test_task_for_eatster/data/network/SlidesResponse.kt
+++ app/src/main/java/com/obriysoft/test_task_for_eatster/data/network/SlidesResponse.kt
@@ -1,5 +1,8 @@
 package com.example.testtaskfore.data.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class SlidesResponse(
     val slides: List<Slide>
 )
--- app/src/main/java/com/obriysoft/test_task_for_eatster/presentation/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/obriysoft/test_task_for_eatster/presentation/screens/home/HomeViewModel.kt
@@ -1,13 +1,21 @@
 package com.obriysoft.test_task_for_eatster.presentation.screens.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.testtaskfore.data.network.SlidesRemoteDataSource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-
+    private val slidesRemoteDataSource: SlidesRemoteDataSource
 ) : ViewModel() {
 
-
+    init {
+        viewModelScope.launch {
+            val slides = slidesRemoteDataSource.getSlides()
+            slides
+        }
+    }
 }
