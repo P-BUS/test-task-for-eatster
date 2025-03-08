12fbd18de19ed9f05a3d6fa266a9264028a8fb10
P-BUS <pavlo.buslowski@gmail.com>
2025-03-08 17:26:25 +0200

    - image URL corrections for coil

--- app/src/main/java/com/obriysoft/test_task_for_eatster/data/model/mapper/SlidesDtoMapper.kt
+++ app/src/main/java/com/obriysoft/test_task_for_eatster/data/model/mapper/SlidesDtoMapper.kt
@@ -2,6 +2,8 @@ package com.obriysoft.test_task_for_eatster.data.model.mapper
 
 import com.example.testtaskfore.data.model.SlidesResponse
 import com.obriysoft.test_task_for_eatster.data.local.database.SlideEntity
+import java.net.URLDecoder
+import java.net.URLEncoder
 
 fun SlidesResponse.toDatabaseModel(): List<SlideEntity> {
     return this.data.map { data ->
@@ -9,7 +11,14 @@ fun SlidesResponse.toDatabaseModel(): List<SlideEntity> {
             id = data.id,
             position = data.position,
             duration = data.duration.toInt(),
-            imageUrl = data.url
+            imageUrl = data.url.urlEncoding()
         )
     }
+}
+
+// TODO: this solution is not working
+private fun String.urlEncoding(): String {
+    val decodedUrl = URLDecoder.decode(this, "UTF-8")
+    val encodedUrl = URLEncoder.encode(decodedUrl, "UTF-8")
+    return encodedUrl
 }
